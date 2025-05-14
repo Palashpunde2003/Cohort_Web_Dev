d1bbe743881f68162c2e94a1259f86c295c82999
Palash Punde <161244532+Palashpunde2003@users.noreply.github.com>
2025-05-15 00:34:03 +0530

    Update todo_backend.js
    Added delete functionality

--- Week_4_Node_Js/Week_4_todo_app/todo_backend.js
+++ Week_4_Node_Js/Week_4_todo_app/todo_backend.js
@@ -9,24 +9,34 @@ app.post('/todos/create', function(req, res){
     const {title,id} = req.body;
     todos.push({title,id});
     res.status(201).json({message: 'Todo added succesfully'});
-})
+});
 
 app.delete('/todos/delete/:id', function(req, res){
     const todoId = Number(req.params.id);
-    for(let i=0; i<todos.length; i++){
-        if(todos[i].id == todoId){
+    let deleted = false;
+    for(let i = 0; i < todos.length; i++){
+        if (todos[i].id === todoId) {
             todos.splice(i,1);
+            deleted = true;
             break;
         }
     }
+    todos.forEach((todo,index)=> {
+        todo.id = index+1;
+    });
+
     console.log(todos);
-    res.status(201).json({message: 'Todo removed succesfully'});
-})
+    if(deleted){
+        res.status(201).json({message: 'Todo removed succesfully'});
+    } else {
+        res.status(404).json({ message: 'Todo not found' });
+    }
+});
 
 app.get('/todos/read',function(req,res){
     let responseText = todos.map(todo => `${todo.id}. ${todo.title}`).join('<br>');
     res.send(`<h1>${responseText}</h1>`);
-})
+});
 
 app.listen(3000, (error) =>{
         if(!error){
