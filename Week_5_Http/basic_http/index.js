3c7d4c0334fbd939a124fe2481694829f4646f91
Palash Punde <161244532+Palashpunde2003@users.noreply.github.com>
2025-05-18 23:29:28 +0530

    Create index.js

---
+++ Week_5_Http/basic_http/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+app.get('/multiply', function(req,res){
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    res.json({
+        ans : a*b
+    })
+});
+
+app.get('/sum', function(req,res){
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    res.json({
+        ans : a+b
+    })
+});
+
+app.get('/divide', function(req,res){
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    res.json({
+        ans : a/b
+    })
+});
+
+app.get('/substract', function(req,res){
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    res.json({
+        ans : a-b
+    })
+});
+
+app.listen(3000);
